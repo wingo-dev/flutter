3cff74858e7e849f4bf371cf3374bc180b5d9803
Li <dongj385@gmail.com>
2023-08-09 02:33:42 -0500

    center widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Text("hello world!")));
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("hello world!"),
+        ),
+      ),
+    ),
+  );
 }
