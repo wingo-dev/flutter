b7fd02c72f943a1ee256a145fa62252bab379ff9
Li <dongj385@gmail.com>
2023-08-09 03:05:30 -0500

    text style, background color

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,29 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text("hello world!"),
+        backgroundColor: Colors.deepPurple,
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 70, 38, 146),
+                Color.fromRGBO(233, 68, 35, 1),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              "hello world!",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ),
+            ),
+          ),
         ),
       ),
     ),
