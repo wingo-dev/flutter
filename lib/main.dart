9670c3be6ed28f37ec51bc763146f36dfc5a190e
Li <dongj385@gmail.com>
2023-08-10 04:11:15 -0500

    splited files

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trial_app/gradient_container.dart';
 
 void main() {
   runApp(
@@ -9,33 +10,4 @@ void main() {
       ),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 70, 38, 146),
-            Color.fromRGBO(233, 68, 35, 1),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          "Hello World!",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-          ),
-        ),
-      ),
-    );
-    throw UnimplementedError();
-  }
 }
