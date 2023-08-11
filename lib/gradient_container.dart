c3633a7b321244ae8401292a229ff40355583cd4
Li <dongj385@gmail.com>
2023-08-11 03:51:18 -0500

    changed color by dynamic variables

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -2,22 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:trial_app/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+  final Color color1;
+  final Color color2;
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 70, 38, 146),
-            Color.fromRGBO(233, 68, 35, 1),
-          ],
+          colors: [color1, color2],
           begin: Alignment.topLeft,
           end: Alignment.bottomRight,
         ),
       ),
       child: const Center(
-        child: StyledText(),
+        child: StyledText("Wingo dynamic Text!"),
       ),
     );
   }
